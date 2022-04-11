8ccecb84ac43a8b49a90d5a61f19f51a732bdfe9
Hannah <hannahwoodward@hotmail.com>
2022-04-11 13:51:26 +0800

    done

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,9 @@
+# app/controllers/posts_controller.rb
+
+class PostsController < ApplicationController
+    # app/controllers/posts_controller.rb
+
+    def show
+      @post = Post.find(params[:id])
+    end
+end
